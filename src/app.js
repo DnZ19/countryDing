ed54362de8fa84435e1f57e780b09333fffd548a
DnZ19 <dennis@centerofagile.nl>
2022-07-08 11:09:54 +0200

    opdracht af

--- src/app.js
+++ src/app.js
@@ -1,1 +1,45 @@
+import axios from "axios";
+
 console.log('Hallo daar!');
+
+const API_Key = "https://restcountries.com/v2/all";
+const countryList = document.getElementById('country-container');
+
+
+async function fetchCountries(){
+
+    try {
+        const response = await axios.get(API_Key);
+
+        response.data.sort(( a, b) => {
+            return a.population - b.population;
+        });
+
+        const mappedResponse = response.data.map(( country ) => ( {
+                countryRegion: country.region,
+                countryName: country.name,
+                countryPop: country.population,
+                countryFlag: country.flags.png
+        }));
+
+        for (let i = 0; i < mappedResponse.length; i++) {
+            countryList.innerHTML +=
+                `
+            <li class="${mappedResponse[i].countryRegion}">  
+                <h4>${mappedResponse[i].countryName} has a population of: </h4>
+                <p>${mappedResponse[i].countryPop}</p> 
+                <img src="${mappedResponse[i].countryFlag}" alt="Flag">
+            </li>   
+                `
+        }
+
+
+
+    } catch(e) {
+        console.error( e );
+    }
+
+}
+
+fetchCountries();
+
